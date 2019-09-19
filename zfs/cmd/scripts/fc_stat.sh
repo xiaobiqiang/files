22d9135cc23561025661668f98a21939aad9fcf8
ryzhang <ryzhang@ceresdata.net>
2019-09-19 19:55:18 +0800

    代码重新对齐

--- zfs/cmd/scripts/fc_stat.sh
+++ zfs/cmd/scripts/fc_stat.sh
@@ -31,59 +31,59 @@ function stat_main()
 #==================================================================================
 function stat_sys()
 {
-	rm -f ${SYS_FILE}
-    	touch ${SYS_FILE}
-	if [ ! -f "$TARGET_FILE" ]; then
-		touch ${TARGET_FILE}
-	fi
+    rm -f ${SYS_FILE}
+    touch ${SYS_FILE}
+    if [ ! -f "$TARGET_FILE" ]; then
+        touch ${TARGET_FILE}
+    fi
     if [ ! -f "$PROC_NFSD" ]; then
-		local nfs_nr=0
+        local nfs_nr=0
         local nfs_nw=0
     else
         local nfs_nr=`cat $PROC_NFSD |grep "io"|awk -F' ' '{print $2}'`
         local nfs_nw=`cat $PROC_NFSD |grep "io"|awk -F' ' '{print $3}'`
-	fi
-	local sum=(0 0 0 0 0 0 0 0)
-	local fc_name=`cat $TARGET_FILE |grep "wwn"|awk -F' ' '{print $1}'`
-	local nic_name=`cat $PROC_NETDEV |grep ":"|awk -F':' '{print $1}'`
-	
-	for num in $fc_name
-	do
-		reads=`cat $TARGET_FILE|grep -w "$num"|awk -F' ' '{print $4}'`
-		writes=`cat $TARGET_FILE|grep -w "$num" |awk -F' ' '{print $5}'`
-		nread=`cat $TARGET_FILE|grep -w "$num" |awk -F' ' '{print $2}'`
-		nwritten=`cat $TARGET_FILE|grep -w "$num" |awk -F' ' '{print $3}'`
-		((sum[0]=${sum[0]}+$reads))
-		((sum[1]=${sum[1]}+$writes))
-		((sum[2]=${sum[2]}+$nread))
-		((sum[3]=${sum[3]}+$nwritten))
-	done
-	for name in $nic_name
-	do
-		nic_nread=`cat $PROC_NETDEV|grep -w "$name" |awk -F' ' '{print $2}'`
-		nic_nwritten=`cat $PROC_NETDEV|grep -w "$name" |awk -F' ' '{print $10}'`
-		nic_reads=`cat $PROC_NETDEV|grep -w "$name" |awk -F' ' '{print $3}'`
-		nic_writes=`cat $PROC_NETDEV|grep -w "$name" |awk -F' ' '{print $11}'`
-		((sum[4]=$nic_nread+${sum[4]}))
-		((sum[5]=$nic_nwritten+${sum[5]}))
-		((sum[6]=$nic_reads+${sum[6]}))
-		((sum[7]=$nic_writes+${sum[7]}))
-	done
-	echo "${sum[@]} $nfs_nr $nfs_nw">>${SYS_FILE}
+    fi
+    local sum=(0 0 0 0 0 0 0 0)
+    local fc_name=`cat $TARGET_FILE |grep "wwn"|awk -F' ' '{print $1}'`
+    local nic_name=`cat $PROC_NETDEV |grep ":"|awk -F':' '{print $1}'`
+    
+    for num in $fc_name
+    do
+        reads=`cat $TARGET_FILE|grep -w "$num"|awk -F' ' '{print $4}'`
+        writes=`cat $TARGET_FILE|grep -w "$num" |awk -F' ' '{print $5}'`
+        nread=`cat $TARGET_FILE|grep -w "$num" |awk -F' ' '{print $2}'`
+        nwritten=`cat $TARGET_FILE|grep -w "$num" |awk -F' ' '{print $3}'`
+        ((sum[0]=${sum[0]}+$reads))
+        ((sum[1]=${sum[1]}+$writes))
+        ((sum[2]=${sum[2]}+$nread))
+        ((sum[3]=${sum[3]}+$nwritten))
+    done
+    for name in $nic_name
+    do
+        nic_nread=`cat $PROC_NETDEV|grep -w "$name" |awk -F' ' '{print $2}'`
+        nic_nwritten=`cat $PROC_NETDEV|grep -w "$name" |awk -F' ' '{print $10}'`
+        nic_reads=`cat $PROC_NETDEV|grep -w "$name" |awk -F' ' '{print $3}'`
+        nic_writes=`cat $PROC_NETDEV|grep -w "$name" |awk -F' ' '{print $11}'`
+        ((sum[4]=$nic_nread+${sum[4]}))
+        ((sum[5]=$nic_nwritten+${sum[5]}))
+        ((sum[6]=$nic_reads+${sum[6]}))
+        ((sum[7]=$nic_writes+${sum[7]}))
+    done
+    echo "${sum[@]} $nfs_nr $nfs_nw">>${SYS_FILE}
 }
 function main()
 {
-	local paramnum=$#
-	case $1 in 
-		fc_stat)
-		stat_main
-		;;
-		sys)
-		stat_sys
-		;;
-		*)
+    local paramnum=$#
+    case $1 in 
+        fc_stat)
+        stat_main
         ;;
-	esac
+        sys)
+        stat_sys
+        ;;
+        *)
+        ;;
+    esac
 }
 main $*
 exit $?
