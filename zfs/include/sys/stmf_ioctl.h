a48e2555a30995e8f7066e396958ccc41a4917a7
bqxiao <bqxiao@ceresdata.net>
2019-10-30 15:31:22 +0800

    uncomment mirror replay

--- zfs/include/sys/stmf_ioctl.h
+++ zfs/include/sys/stmf_ioctl.h
@@ -163,7 +163,7 @@ typedef struct stmf_alua_state_desc {
 
 typedef struct stmf_lu_state_desc {
 	uint8_t lu_access_state;
-	uint8_t	lu_guid[16];
+	void *lu_sess;
 } stmf_lu_state_desc_t;
 
 typedef struct stmf_lu_task_limit {
