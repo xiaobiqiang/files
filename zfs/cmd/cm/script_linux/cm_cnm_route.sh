0e2278d98cf33b81d886dc49a8a86e1e3489bc4a
熊傲然 <arxiong@ceresdata.net>
2020-01-15 14:30:33 +0800

    CM route适配

--- zfs/cmd/cm/script_linux/cm_cnm_route.sh
+++ zfs/cmd/cm/script_linux/cm_cnm_route.sh
@@ -4,18 +4,13 @@ source '/var/cm/script/cm_common.sh'
 
 function cm_cnm_route_getbatch()
 {
-	local rows=`netstat -nrv|grep '\.'|egrep -v 'lo0'|awk '{print $1":"$2":"$3}'`
-	
-	for row in $rows
-	do
-		echo $row | awk -F':' '{print $1" "$2" "$3}'
-	done
+	route |egrep -v 'Kernel|Destination'|awk '{print $1" "$2" "$3}'
 	return $CM_OK
 }
 
 function cm_cnm_route_count()
 {
-	len=`netstat -rnv|grep '\.'|egrep -v 'lo0'|wc -l`
+	len=`route |egrep -v 'Kernel|Destination'|wc -l`
 	echo $len
 	return $CM_OK
 }
@@ -25,12 +20,12 @@ function cm_cnm_route_create()
 	local destination=$1
 	local netmask=$2
 	local geteway=$3
-	num=`netstat -rv|grep "\."|egrep -v 'default|localhost'|grep "^$destination"|grep "$geteway"|wc -l`
+	num=`route |grep "$destination"|grep "$netmask"|grep "$geteway"|wc -l`
 	if [ $num -ne 0 ]; then
 		CM_LOG "[${FUNCNAME}:${LINENO}] already exsit"
 		return $CM_ERR_ALREADY_EXISTS
 	fi
-	route add -net $destination $geteway -netmask $netmask
+	route add -net $destination netmask $netmask gw $geteway 2>/dev/null
 	local iRet=$?
 	if [ $iRet -ne $CM_OK ]; then
 		CM_LOG "[${FUNCNAME}:${LINENO}] $destination $geteway $netmask fail"
@@ -43,12 +38,12 @@ function cm_cnm_route_delete()
 	local destination=$1
 	local netmask=$2
 	local geteway=$3
-	num=`netstat -rv|grep "\."|egrep -v 'default|localhost'|grep "^$destination"|grep "$geteway"|wc -l`
+	num=`route |grep "$destination"|grep "$netmask"|grep "$geteway"|wc -l`
 	if [ $num -eq 0 ]; then
 		CM_LOG "[${FUNCNAME}:${LINENO}] no exsit"
 		return $CM_ERR_NOT_EXISTS
 	fi
-	route delete -net $destination $geteway -netmask $netmask
+	route delete -net $destination netmask $netmask gw $geteway 2>/dev/null
 	local iRet=$?
 	if [ $iRet -ne $CM_OK ]; then
 		CM_LOG "[${FUNCNAME}:${LINENO}] $destination $geteway $netmask fail"
