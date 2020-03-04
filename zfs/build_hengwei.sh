70b29e3870b6fa89e6ff2a615f453f7f1091b69a
bqxiao <bqxiao@ceresdata.net>
2020-03-04 18:12:57 +0800

    mantis 8116 (3)

--- zfs/build_hengwei.sh
+++ zfs/build_hengwei.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cp module/Makefile_hengwei.in module/Makefile.in
-cp etc/systemd/system/fc-boot.service.in_hengwei etc/systemd/system/fc-boot.service.in
 ./autogen.sh
 ./configure --enable-hengwei=yes --with-spl=$1
 make -j 16 && make install
