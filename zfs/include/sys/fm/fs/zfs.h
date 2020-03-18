875b1924eeb29b76b32611d5548f0673a84613dc
cyong <cyong@ceresdata.net>
2020-03-18 15:42:43 +0800

    watch noresponse commands;

--- zfs/include/sys/fm/fs/zfs.h
+++ zfs/include/sys/fm/fs/zfs.h
@@ -65,6 +65,8 @@ extern "C" {
 
 #define	FM_EREPORT_ZFS_DEVICE_MERR		"vdev.merr"
 #define	FM_EREPORT_ZFS_DEV_REMOVED	  "dev.removed"
+#define	FM_EREPORT_ZFS_DEV_NORESP	    "dev.noresponse"
+
 
 
 #define	FM_EREPORT_PAYLOAD_ZFS_POOL		"pool"
