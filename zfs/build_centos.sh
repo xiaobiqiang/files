5a5e2c782d4c1cfffdc97cb05e07f24670b3e5e6
bqxiao <bqxiao@ceresdata.net>
2020-03-31 17:00:05 +0800

    fix libnetsnmp.so and libnetsnmpagent.so not found

--- zfs/build_centos.sh
+++ zfs/build_centos.sh
@@ -20,25 +20,11 @@ if [ ! -d $SNMP_HEADERS ];then
     rm -rf ./net-snmp/
 fi
 
-
-location_check=`ls -l /usr/lib|grep libnetsnmp|wc -l`
-if [ $location_check -eq 0 ];then
-    location="/usr/lib64"
-else
-    location="/usr/lib"
-fi
-
-if [ ! -f "$location/libnetsnmp.so" ];then
-    libnetsnmp_ver=`ls -l $location|grep -w "libnetsnmp.so"|grep -v ">"|awk '{print $9}'`
-    ln -s $location/$libnetsnmp_ver $location/libnetsnmp.so
-fi
-
-if [ ! -f "$location/libnetsnmpagent.so" ];then
-    libnetsnmpagent_ver=`ls -l $location|grep -w "libnetsnmpagent.so"|grep -v ">"|awk '{print $9}'`
-    ln -s $location/$libnetsnmpagent_ver $location/libnetsnmpagent.so
-fi
-
-
+netsnmp_so_loc=/usr/lib64
+netsnmp_so="libnetsnmp.so libnetsnmpagent.so"
+for so in ${netsnmp_so}; do
+	[ ! -f ${netsnmp_so_loc}/${so} ] && ln -s `ls ${netsnmp_so_loc}/ | grep ${so}.[0-9][0-9].[0-9].` ${netsnmp_so_loc}/${so}
+done
 
 cp module/Makefile_centos.in module/Makefile.in
 ./autogen.sh
