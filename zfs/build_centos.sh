c586b55e5382ae48ba84bd75cc323682f7ab6126
熊傲然 <arxiong@ceresdata.net>
2020-04-10 10:50:36 +0800

    configure指定sbin参数

--- zfs/build_centos.sh
+++ zfs/build_centos.sh
@@ -28,5 +28,5 @@ done
 
 cp module/Makefile_centos.in module/Makefile.in
 ./autogen.sh
-./configure --with-spl=$1 --prefix=
+./configure --with-spl=$1 --prefix=/usr --sbindir=/sbin
 make && make install
