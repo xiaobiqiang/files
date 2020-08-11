4e5a0c64ca313d04962920df364596d56593b45f
bqxiao <bqxiao@ceresdata.net>
2020-08-11 13:17:35 +0800

    feiteng deb

--- zfs/rpminstall/mrpm.sh
+++ zfs/rpminstall/mrpm.sh
@@ -1,50 +1,54 @@
 #!/bin/bash
 
-
 SPL_DIR="../../spl"
 ZFS_DIR=".."
 RPM_DIR="rpm"
 SCSI_DIR="../cmd/scsi_tools"
 SYSTEM_DIR="../etc/systemd/system"
-IS_DEEP=`uname -r|grep deepin|wc -l`
+HAS_DPKG=`which dpkg | wc -l`
 
+OS_RELEASE_DEEPIN="deepin"	#deepin
+OS_RELEASE_CENTOS="centos"	#centos
+OS_RELEASE_NEOKYLIN="neokylin"	#neokylin
+OS_RELEASE_KYLIN="kylin"	#kylin
+OS_RELEASE=`cat /etc/os-release | grep -w ID | sed 's/"//g' | cut -d '=' -f 2`
 
-if [ ! -d $RPM_DIR ]; then
-	mkdir  -p $RPM_DIR/zfs
-	mkdir  -p $RPM_DIR/spl
-	mkdir  -p scsi/lib     
-fi
+[ -d $RPM_DIR ] && rm -rf $RPM_DIR
+mkdir -p $RPM_DIR/zfs
+mkdir -p $RPM_DIR/spl
+mkdir -p scsi/lib
 
+UNAME_FLAG=
+[ $HAS_DPKG -eq 1 ] && UNAME_FLAG=`dpkg --print-architecture`
+[ $HAS_DPKG -eq 0 ] && UNAME_FLAG=`lscpu | awk 'NR==1 {print $2}'`
+[ -z $UNAME_FLAG ] && echo "Can not get system architecture..."
+[ -z $UNAME_FLAG ] && exit 1
 
-UNAME_FLAG=""
-if [ `uname -a |grep x86_64|wc -l` -eq 0 ]; then
-	UNAME_FLAG="sw_64"
+if [[ $OS_RELEASE == $OS_RELEASE_DEEPIN ]] || [[ $OS_RELEASE == $OS_RELEASE_KYLIN ]]; then
+	cd $ZFS_DIR
+
+	cm_rpm=`ls | grep libcm*.rpm`
+	cm_deb=`ls | grep libcm*.deb`
+	[ ! -z $cm_deb ] && rm -f $cm_deb
+	[ -z $cm_rpm ] && echo 'None libcm rpm package...'
+	[ -z $cm_rpm ] && exit 2
+
+	alien --target=$UNAME_FLAG $cm_rpm
+
+	cd -
+	cp $SPL_DIR/*.deb $RPM_DIR/spl
+	cp $ZFS_DIR/*.deb $RPM_DIR/zfs
+
+	mkdir ./system
+	cp $SYSTEM_DIR/*service system
+	cp $SYSTEM_DIR/50-zfs.preset system
+	cp $SYSTEM_DIR/zfs.target system    
+elif [[ $OS_RELEASE == $OS_RELEASE_CENTOS ]] || [[ $OS_RELEASE == $OS_RELEASE_NEOKYLIN ]]; then
+	cp $SPL_DIR/*.$UNAME_FLAG.rpm $RPM_DIR/spl
+	cp $ZFS_DIR/*.$UNAME_FLAG.rpm $RPM_DIR/zfs
 else
-	UNAME_FLAG="x86_64"
-fi
-
-if [ $IS_DEEP -eq 0 ]; then
-    cp $SPL_DIR/*.$UNAME_FLAG.rpm $RPM_DIR/spl
-    cp $ZFS_DIR/*.$UNAME_FLAG.rpm $RPM_DIR/zfs
-else
-    cd $ZFS_DIR
-    if [ `ls |grep libcm|grep deb|wc -l` -eq 0 ]; then
-        cm_rpm=`ls |grep libcm|grep rpm`
-        alien $cm_rpm
-    else
-        cm_deb=`ls |grep libcm|grep deb`
-        cm_rpm=`ls |grep libcm|grep rpm`
-        rm -rf $cm_deb
-        alien $cm_rpm
-    fi
-    cd -
-    cp $SPL_DIR/*.deb $RPM_DIR/spl
-    cp $ZFS_DIR/*.deb $RPM_DIR/zfs
-
-    mkdir ./system
-    cp $SYSTEM_DIR/*service system
-    cp $SYSTEM_DIR/50-zfs.preset system
-    cp $SYSTEM_DIR/zfs.target system    
+	echo "Unsupported System --> $OS_RELEASE"
+	exit 2
 fi
 
 cd $SCSI_DIR
